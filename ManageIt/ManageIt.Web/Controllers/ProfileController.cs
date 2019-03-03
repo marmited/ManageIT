b7303e1ad0f2ba498bc4f13430ae8758068fd601
marmited <37150863+marmited@users.noreply.github.com>
2019-03-03 01:12:39 +0100

    Add home page, layouts

---
+++ ManageIt/ManageIt.Web/Controllers/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManageIt.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+
+        public ProfileController()
+        {
+        }
+
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
